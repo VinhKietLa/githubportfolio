26edb98b547c8b18d1f659e30689a7801be8b71e
VinhKietLa <47901573+VinhKietLa@users.noreply.github.com>
2019-08-02 17:22:53 +0100

    Mobile styles complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+
+//A function to toggle my work experience responsibilities//
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
